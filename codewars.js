a3398e927b68f10c892ff3de61bd68126a3384c3
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-09-07 13:13:45 -0400

    fourth exercise completed

--- codewars.js
+++ codewars.js
@@ -59,7 +59,15 @@ function flyBy(lamps, drone){
 // Get the mean of an array
 // https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
 
-
+function getAverage(marks){
+    let total = 0;
+    let counter = 0;
+    for (let i = 0; i < marks.length; i++) {
+      counter++
+      total = total + marks[i];
+    }
+    return Math.floor(total/counter);
+  }
 
 
 // #5
